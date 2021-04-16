e058f241b98618b72a4050835d2ba602449ce0e3
ShadowFox <641191800@qq.com>
2021-04-16 10:26:04 +0800

    update

--- inventory/dl_data.py
+++ inventory/dl_data.py
@@ -92,7 +92,7 @@ def download_icons():
 
 def save_img(item_id, item_name, img_url):
     if img_url == '':
-        return True
+        return False
     if not os.path.exists(collect_path + item_id):
         os.mkdir(collect_path + item_id)
     filepath = collect_path + item_id + '/%s.png' % item_name

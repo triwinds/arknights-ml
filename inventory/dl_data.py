8305d245e37f18ee4fc41af876d5694fc5bc6a0d
triwinds <7413161+triwinds@users.noreply.github.com>
2021-07-17 23:04:43 +0800

    download latest event icons from wiki event page

--- inventory/dl_data.py
+++ inventory/dl_data.py
@@ -1,11 +1,13 @@
-import requests
-import bs4
-import os
-import json
 import hashlib
+import json
+import os
+import re
 import shutil
-from retry import retry
+from functools import lru_cache
 
+import bs4
+import requests
+from retry import retry
 
 collect_path = 'images/collect/'
 
@@ -66,6 +68,7 @@ def get_items_id_map():
     return res
 
 
+@lru_cache(1)
 def get_items_name_map():
     res = {}
     for item in items:
@@ -75,7 +78,13 @@ def get_items_name_map():
 
 def download_icons():
     update_items()
-    items_name_map = get_items_name_map()
+    flag1 = download_from_items_page()
+    flag2 = download_latest_event_icons()
+    return flag1 and flag2
+
+
+def download_from_items_page():
+    print('checking item page...')
     resp = request_get('http://prts.wiki/w/%E9%81%93%E5%85%B7%E4%B8%80%E8%A7%88')
     soup = bs4.BeautifulSoup(resp.text, features='html.parser')
     data_devs = soup.find_all("div", {"class": "smwdata"})
@@ -85,22 +94,59 @@ def download_icons():
     update_flag = False
     for data_dev in data_devs:
         item_name = data_dev['data-name']
-        item = items_name_map.get(item_name)
-        item_id = 'other'
-        if item and item['itemType'] in ['MATERIAL', 'ARKPLANNER', 'ACTIVITY_ITEM']:
-            item_id = item['itemId']
-            if item['itemType'] != 'ACTIVITY_ITEM' and not item_id.isdigit() or len(item_id) < 5:
-                item_id = 'other'
-        flag = save_img(item_id, item_name, data_dev['data-file'])
+        flag = save_img(item_name, data_dev['data-file'])
         if flag:
             update_flag = True
-            print(item)
+            print(item_name)
         c += 1
         # print(f'{c}/{total} {item_name}')
     return update_flag
 
 
-def save_img(item_id, item_name, img_url):
+def download_latest_event_icons():
+    print('checking event page...')
+    resp = request_get('http://prts.wiki/w/%E9%A6%96%E9%A1%B5')
+    soup = bs4.BeautifulSoup(resp.text, features='html.parser')
+    event_menu = soup.find_all(text='当前活动')
+    update_flag = False
+    if event_menu:
+        li = event_menu[0].parent.parent
+        a_list = li.find_all('a')
+        for a_tag in a_list:
+            event_url = 'http://prts.wiki' + a_tag['href']
+            print('handle event:', a_tag.text)
+            flag = download_from_event_page(event_url)
+            if flag:
+                update_flag = True
+    return update_flag
+
+
+def download_from_event_page(event_url):
+    resp = request_get(event_url)
+    soup = bs4.BeautifulSoup(resp.text, features='html.parser')
+    item_imgs = soup.find_all('img', attrs={'alt': re.compile('道具')})
+    item_set = set()
+    update_flag = False
+    for item_img in item_imgs:
+        if item_img['alt'] in item_set:
+            continue
+        item_set.add(item_img['alt'])
+        item_name = item_img.parent['title']
+        img_url = 'http://prts.wiki' + item_img['data-srcset'].split(', ')[-1][:-3]
+        flag = save_img(item_name, img_url)
+        if flag:
+            update_flag = True
+    return update_flag
+
+
+def save_img(item_name, img_url):
+    items_name_map = get_items_name_map()
+    item = items_name_map.get(item_name)
+    item_id = 'other'
+    if item and item['itemType'] in {'MATERIAL', 'ARKPLANNER', 'ACTIVITY_ITEM'}:
+        item_id = item['itemId']
+        if item['itemType'] != 'ACTIVITY_ITEM' and not item_id.isdigit() or len(item_id) < 5:
+            item_id = 'other'
     if img_url == '':
         print(f'skip {item_name}, img_url: {img_url}')
         return False
@@ -126,3 +172,5 @@ def save_img(item_id, item_name, img_url):
 
 if __name__ == '__main__':
     download_icons()
+    # print(download_latest_event_icons())
+    # download_from_event_page('http://prts.wiki/w/%E5%AF%86%E6%9E%97%E6%82%8D%E5%B0%86%E5%BD%92%E6%9D%A52021#%E5%A4%A7%E9%85%8B%E9%95%BF%E4%B9%8B%E8%B7%AF')
